f6e2338709c38394124af59c3f9f5861dacc1f7e
mikachelya <mikhail.lukash1@mail.ru>
2025-02-11 16:30:32 +0100

    Beginnings of big refactor
    Made getting legal moves and other chess-related functions into methods of the Board class

--- classes.js
+++ classes.js
@@ -2,7 +2,260 @@ class Board {
     constructor(pieceArray, currentMove = WHITE, enPassantTarget = null) {
         this.pieceArray = pieceArray;
         this.enPassantTarget = enPassantTarget;
-        this.currentMove = currentMove
+        this.currentMove = currentMove;
+        this.deletedPiece;
+        this.lastMove;
+        this.lastEnPassantTarget = undefined;
+    }
+
+    getLegalMovesSimple(r, c, excludeChecks = true) {
+        let resultArr = [];
+        
+        if (!this.pieceArray[r][c])
+            return resultArr;
+        
+        let currentPiece = this.pieceArray[r][c]
+        let currentPieceType = currentPiece.typeArray[0];
+        let currentColour = currentPiece.colour;
+        
+        if ([ROOK, QUEEN, BISHOP].includes(currentPieceType)) {
+            let moveDirections = SLIDINGPIECEMOVES[currentPieceType];
+            for (let dir = 0; dir < moveDirections[0].length; dir++) {
+                let currentR = r;
+                let currentC = c;
+                while (true) {
+                    currentR += moveDirections[0][dir];
+                    currentC += moveDirections[1][dir];
+                    
+                    if (currentR >= 8 || currentR < 0 || currentC >= 8 || currentC < 0)
+                        break;
+                    
+                    if (this.pieceArray[currentR][currentC]) {
+                        if (this.pieceArray[currentR][currentC].colour != currentColour)
+                            resultArr.push([currentR, currentC]);
+                        break;
+                    }
+
+                    resultArr.push([currentR, currentC]);
+                }
+            }
+        }
+
+        else if ([KING, KNIGHT].includes(currentPieceType)) {
+            let moveDirections = JUMPINGPIECEMOVES[currentPieceType];
+            for (let dir = 0; dir < moveDirections[0].length; dir++) {
+                let currentR = r + moveDirections[0][dir];
+                let currentC = c + moveDirections[1][dir];
+                
+                if (currentR >= 8 || currentR < 0 || currentC >= 8 || currentC < 0)
+                    continue;
+                
+                if (this.pieceArray[currentR][currentC]) {
+                    if (this.pieceArray[currentR][currentC].colour != currentColour)
+                        resultArr.push([currentR, currentC]);
+                    continue;
+                }
+
+                resultArr.push([currentR, currentC]);
+            }
+
+            // castling
+            if (currentPieceType == KING && !currentPiece.hasMoved && excludeChecks) {
+                // find adjascent rooks
+                let castlingPartners = {};
+                let testC;
+                for (let offset of [-1, 1]) {
+                    for (testC = c + offset; testC >= 0 && testC < 8; testC += offset)
+                        if (this.pieceArray[r][testC])
+                            break;
+
+                    // first piece has to be a rook of the same colour which hasn't moved
+                    if (testC >= 0 && testC < 8 
+                        && this.pieceArray[r][testC].typeArray[0] == ROOK
+                        && this.pieceArray[r][testC].colour == currentColour
+                        && this.pieceArray[r][testC].hasMoved == false)
+                        castlingPartners[offset] = testC;
+                }
+
+                // filter out illegal castling
+                castlingPartners = Object.entries(castlingPartners).filter(
+                    ([dir, rookStart]) => {
+                        let [kingDest, rookDest] = CASTLINGDESTINATIONS[dir];
+                        // illegal if king would move through check
+                        for (let i of range(c, kingDest, dir))
+                            if (this.isCheck(currentColour, [r, i]))
+                                return false;
+
+                        let relevantSquares = [c, kingDest, rookStart, rookDest];
+                        // illegal if king or rook would move through another piece
+                        for (let i of range(min(relevantSquares), max(relevantSquares))) {
+                            // todo: skipping king/rook destination squares is probably wrong
+                            if (relevantSquares.includes(i))
+                                continue;
+                            if (this.pieceArray[r][i])
+                                return false;
+                        }
+                        return true;
+                    }
+                )
+
+                // add the square where the rook is to the legal moves
+                for (let keyVal of castlingPartners)
+                    resultArr.push([r, keyVal[1]]);
+            }
+        }
+
+        else if (currentPieceType == PAWN && r != 0 && r != 7) {
+            let direction = (currentColour == WHITE ? -1 : 1);
+            // if the square in front is free, you can go there
+            if (!this.pieceArray[r + direction][c]) {
+                resultArr.push([r + direction, c]);
+
+                // if the square in front of that one is also free, 
+                // and the pawn hasn't moved yet, you can go there
+                if ((currentColour == WHITE && r == 6 || currentColour == BLACK && r == 1) &&
+                    !this.pieceArray[r + direction * 2][c])
+                    resultArr.push([r + direction * 2, c]);
+            }
+
+            // capture diagonally (including en passant)
+            for (let offsets of [[direction, -1], [direction, 1]]) {
+                let nextR = r + offsets[0];
+                let nextC = c + offsets[1];
+                if (nextR >= 8 || nextR < 0 || nextC >= 8 || nextC < 0)
+                    //return;
+                    continue;
+
+                if (this.pieceArray[nextR][nextC] && 
+                    this.pieceArray[nextR][nextC].colour != currentColour ||
+                    this.enPassantTarget &&
+                    this.enPassantTarget[0] == nextR && this.enPassantTarget[1] == nextC)
+                    resultArr.push([nextR, nextC]);
+            }
+        }
+        
+        // filter out moves that would leave the king in check
+        if (excludeChecks)
+            resultArr = resultArr.filter(move => {
+                // todo: add en passant handling
+                let temp = this.pieceArray[move[0]][move[1]];
+                // skip these checks for castling, as that has it's own logic
+                if (temp && temp.colour == currentColour)
+                    return true;
+                // make the move
+                this.pieceArray[move[0]][move[1]] = currentPiece;
+                this.pieceArray[r][c] = undefined;
+                // check for a check
+                let isValid = !this.isCheck(this.currentColour);
+                // undo the move
+                this.pieceArray[r][c] = currentPiece;
+                this.pieceArray[move[0]][move[1]] = temp;
+                return isValid;
+            });
+
+        return resultArr;
+    }
+
+    isCheck(colour, kingCoords = null) {
+        if (!kingCoords) {
+            let kingFound = false;
+            for (let r = 0; r < 8 && !kingFound; r++) {
+                for (let c = 0; c < 8; c++) {
+                    if (this.pieceArray[r][c] && this.pieceArray[r][c].colour == colour && 
+                        this.pieceArray[r][c].typeArray[0] == KING) {
+                        kingCoords = [r, c];
+                        kingFound = true;
+                        break;
+                    }
+                }
+            }   
+    
+            if (!kingCoords)
+                return false;
+        }
+    
+        for (let r = 0; r < 8; r++)
+            for (let c = 0; c < 8; c++)
+                if (this.pieceArray[r][c] && this.pieceArray[r][c].colour != colour)
+                    if (this.getLegalMovesSimple(r, c, false).some(e => e[0] == kingCoords[0] && e[1] == kingCoords[1]))
+                        return true;
+    
+        return false
+    }
+
+    makeMove(sourceR, sourceC, targetR, targetC) {
+        let currentPiece = this.pieceArray[sourceR][sourceC];
+        this.deletedPiece = undefined;
+
+        let legalMoves = this.getLegalMovesSimple(sourceR, sourceC);
+        if (legalMoves.some(([r, c]) => r == targetR && c == targetC)) {
+            currentPiece.hasMoved = true;
+            
+            // castling
+            if (this.pieceArray[targetR][targetC] &&
+                this.pieceArray[targetR][targetC].colour == currentPiece.colour) {
+                this.pieceArray[currentPiece.r][currentPiece.c] = undefined;
+                let rook = this.pieceArray[targetR][targetC];
+                this.pieceArray[targetR][targetC] = undefined;
+                let direction = targetC < currentPiece.c ? "-1" : "1";
+                let [kingDest, rookDest] = CASTLINGDESTINATIONS[direction];
+                this.pieceArray[targetR][rookDest] = rook;
+                this.pieceArray[targetR][kingDest] = currentPiece;
+            }
+            else {
+                this.deletePiece(targetR, targetC);
+                this.pieceArray[targetR][targetC] = currentPiece;
+                this.pieceArray[currentPiece.r][currentPiece.c] = undefined;
+            }
+            
+            // en passant
+            if (this.enPassantTarget && 
+                currentPiece.typeArray[0] == PAWN && 
+                targetR == this.enPassantTarget[0] &&
+                targetC == this.enPassantTarget[1])
+                this.deletePiece(targetR - (this.currentMove == WHITE ? -1 : 1), targetC);
+            
+            // update en passant target
+            if (currentPiece.typeArray[0] == PAWN && abs(currentPiece.r - targetR) == 2)
+                this.enPassantTarget = [(currentPiece.r + targetR) / 2, targetC];
+            else {
+                this.lastEnPassantTarget = this.enPassantTarget;
+                this.enPassantTarget = undefined;
+            }
+        
+            // promote pawn to queen for now  
+            if (currentPiece.typeArray[0] == PAWN && [0, 7].includes(targetR))
+                this.pieceArray[targetR][targetC].typeArray[0] = QUEEN;
+        
+            // swap the active side
+            this.currentMove = 1 - this.currentMove;
+            this.lastMove = [sourceR, sourceC, targetR, targetC];
+
+            return true;
+        }
+        return false;
+    }
+
+    undoMove() {
+        this.pieceArray[this.lastMove[0]][this.lastMove[1]] 
+            = this.pieceArray[this.lastMove[2]][this.lastMove[3]];
+        this.pieceArray[this.lastMove[2]][this.lastMove[3]] = undefined;
+        if (this.deletedPiece)
+            this.pieceArray[this.deletedPiece.r][this.deletedPiece.c] = this.deletedPiece;
+        this.currentMove = 1 - this.currentMove;
+        this.enPassantTarget = this.lastEnPassantTarget;
+    }
+
+    deletePiece(r, c) {
+        if (!this.pieceArray[r][c]) {
+            this.deletedPiece = undefined;
+            return;
+        }
+
+        this.deletedPiece = this.pieceArray[r][c];
+        this.deletedPiece.r = r;
+        this.deletedPiece.c = c;
+        this.pieceArray[r][c] = undefined;
     }
 }
 
@@ -15,13 +268,3 @@ class Piece {
         this.r; this.c;
     }
 }
-
-
-// class HeldPiece extends Piece {
-//     construcor(typeArray, colour, r, c) {
-//         super(typeArray);
-//         super(colour);
-//         this.r = r;
-//         this.c = c;
-//     }
-// }
--- sketch.js
+++ sketch.js
@@ -5,232 +5,204 @@ let GREEN = [12, 110, 51];
 let GREENFILL = [...GREEN, 50];
 let GREENSTROKE = [...GREEN, 150];
 
+document.oncontextmenu = _ => false;
+
+
 function preload() {
-  PIECES = [
-    [
-      loadImage("resources/bW.png"),
-      loadImage("resources/kW.png"),
-      loadImage("resources/nW.png"),
-      loadImage("resources/pW.png"),
-      loadImage("resources/qW.png"),
-      loadImage("resources/rW.png"),
-    ],
-    [
-      loadImage("resources/bB.png"),
-      loadImage("resources/kB.png"),
-      loadImage("resources/nB.png"),
-      loadImage("resources/pB.png"),
-      loadImage("resources/qB.png"),
-      loadImage("resources/rB.png"),
+    PIECES = [
+        [
+            loadImage("resources/bW.png"),
+            loadImage("resources/kW.png"),
+            loadImage("resources/nW.png"),
+            loadImage("resources/pW.png"),
+            loadImage("resources/qW.png"),
+            loadImage("resources/rW.png"),
+        ],
+        [
+            loadImage("resources/bB.png"),
+            loadImage("resources/kB.png"),
+            loadImage("resources/nB.png"),
+            loadImage("resources/pB.png"),
+            loadImage("resources/qB.png"),
+            loadImage("resources/rB.png"),
+        ]
     ]
-  ]
 }
 
 
 function setup() {
-  
-  canvasWidth = min(windowHeight, windowWidth) - 20;
-  createCanvas(canvasWidth, canvasWidth);
+    
+    canvasWidth = min(windowHeight, windowWidth) - 20;
+    createCanvas(canvasWidth, canvasWidth);
 
-  let grid = [...Array(8)].map(_ => Array(8));
+    let grid = [...Array(8)].map(_ => Array(8));
 
-  let config = STARTINGFEN.split(" ")[0].split("/");
-  let startingRows = [0, 1, 6, 7];
-  for (let row in config) {
-    for (let letter in config[row]) {
-      grid[startingRows[row]][letter] =
-        new Piece([LETTERTOPIECE[config[row][letter].toLowerCase()]],
-          config[row][letter] == config[row][letter].toUpperCase() ? WHITE : BLACK);
+    let config = STARTINGFEN.split(" ")[0].split("/");
+    let startingRows = [0, 1, 6, 7];
+    for (let row in config) {
+        for (let letter in config[row]) {
+            grid[startingRows[row]][letter] =
+                new Piece([LETTERTOPIECE[config[row][letter].toLowerCase()]],
+                    config[row][letter] == config[row][letter].toUpperCase() ? WHITE : BLACK);
+        }
     }
-  }
 
-  // grid[0][5] = new Piece([KING], WHITE);
-  // grid[0][0] = new Piece([ROOK], WHITE);
-  // grid[0][6] = new Piece([ROOK], WHITE);
-  // grid[7][5] = new Piece([ROOK], BLACK);
-  // grid[2][5] = new Piece([PAWN], BLACK);
-  // //grid[3][3] = new Piece([BISHOP], WHITE);
-  // grid[7][4] = new Piece([KNIGHT], BLACK);
-  // //grid[6][4] = new Piece([ROOK], WHITE);
-  // grid[2][2] = new Piece([PAWN], WHITE);
-  // grid[7][7] = new Piece([KING], BLACK);
-  // grid[7][0] = new Piece([QUEEN], BLACK);
-  mainBoard = new Board(grid);
-  squareWidth = canvasWidth / 8;
+    // grid[0][5] = new Piece([KING], WHITE);
+    // grid[0][0] = new Piece([ROOK], WHITE);
+    // grid[0][6] = new Piece([ROOK], WHITE);
+    // grid[7][5] = new Piece([ROOK], BLACK);
+    // grid[2][5] = new Piece([PAWN], BLACK);
+    // //grid[3][3] = new Piece([BISHOP], WHITE);
+    // grid[7][4] = new Piece([KNIGHT], BLACK);
+    // //grid[6][4] = new Piece([ROOK], WHITE);
+    // grid[2][2] = new Piece([PAWN], WHITE);
+    // grid[7][7] = new Piece([KING], BLACK);
+    // grid[7][0] = new Piece([QUEEN], BLACK);
+    mainBoard = new Board(grid);
+    squareWidth = canvasWidth / 8;
 
-  // console.log("White is in check: ", isCheck(mainBoard, WHITE));
-  // console.log("Black is in check: ", isCheck(mainBoard, BLACK));
+    // console.log("White is in check: ", isCheck(mainBoard, WHITE));
+    // console.log("Black is in check: ", isCheck(mainBoard, BLACK));
 }
 
 
 function draw() {
-  drawBoard(mainBoard);
-  drawLegalMoves();
-  drawHeldPiece();
+    drawBoard(mainBoard);
+    drawLegalMoves();
+    drawHeldPiece();
 }
 
 
 function drawBoard(board) {
-  push();
-  strokeWeight(0);
-  let light = true;
-  for (let r = 0; r < 8; r++) {
-      for (let c = 0; c < 8; c++) {
-          light = (r + c) % 2
-          fill(...[light ? [140, 162, 173] : [222, 227, 230]]);
-          square(c * squareWidth, r * squareWidth, squareWidth);
-          if (board.pieceArray[r][c]) {
-            drawPiece(r, c, board.pieceArray[r][c]);
-          }
-      }
-  }
+    push();
+    strokeWeight(0);
+    let light = true;
+    for (let r = 0; r < 8; r++) {
+            for (let c = 0; c < 8; c++) {
+                    light = (r + c) % 2
+                    fill(...[light ? [140, 162, 173] : [222, 227, 230]]);
+                    square(c * squareWidth, r * squareWidth, squareWidth);
+                    if (board.pieceArray[r][c]) {
+                        drawPiece(r, c, board.pieceArray[r][c]);
+                    }
+            }
+    }
 
-  pop();
+    pop();
 }
 
 
 function windowResized() {
-  canvasWidth = min(windowHeight, windowWidth) - 20;
-  squareWidth = canvasWidth / 8;
-  resizeCanvas(canvasWidth, canvasWidth);
+    canvasWidth = min(windowHeight, windowWidth) - 20;
+    squareWidth = canvasWidth / 8;
+    resizeCanvas(canvasWidth, canvasWidth);
 }
 
 
 function drawPiece(r, c, piece) {
-  drawPieceScreen(r * squareWidth, c * squareWidth, piece);
+    drawPieceScreen(r * squareWidth, c * squareWidth, piece);
 }
 
 
 function drawPieceScreen(rpx, cpx, piece) {
-  if (piece.typeArray.length == 1)
-    image(PIECES[piece.colour][piece.typeArray[0]],
-      cpx, rpx, squareWidth, squareWidth);
+    if (piece.typeArray.length == 1)
+        image(PIECES[piece.colour][piece.typeArray[0]],
+            cpx, rpx, squareWidth, squareWidth);
 }
 
 
 function drawHeldPiece() {
-  if (!heldPiece)
-    return;
+    if (!heldPiece)
+        return;
 
-  drawTransparentSquare(heldPiece.r, heldPiece.c);
-  drawPieceScreen(mouseY - squareWidth / 2, mouseX - squareWidth / 2, heldPiece);
+    drawTransparentSquare(heldPiece.r, heldPiece.c);
+    drawPieceScreen(mouseY - squareWidth / 2, mouseX - squareWidth / 2, heldPiece);
 }
 
 function drawTransparentSquare(r, c, outlineOnly = false) {
-  push();
-  noFill();
-  noStroke();
-  lineWidth = squareWidth / 10
-  strokeWeight(lineWidth);
-  if (outlineOnly) {
-    stroke(GREENSTROKE);
-    square(
-      c * squareWidth + lineWidth / 2,
-      r * squareWidth + lineWidth / 2,
-      squareWidth - lineWidth
-    );
-  }
-  else {
-    fill(GREENFILL);
-    square(
-      c * squareWidth,
-      r * squareWidth,
-      squareWidth
-    );
-  }
-  pop();
+    push();
+    noFill();
+    noStroke();
+    lineWidth = squareWidth / 10
+    strokeWeight(lineWidth);
+    if (outlineOnly) {
+        stroke(GREENSTROKE);
+        square(
+            c * squareWidth + lineWidth / 2,
+            r * squareWidth + lineWidth / 2,
+            squareWidth - lineWidth
+        );
+    }
+    else {
+        fill(GREENFILL);
+        square(
+            c * squareWidth,
+            r * squareWidth,
+            squareWidth
+        );
+    }
+    pop();
 }
 
 
 function mousePressed() {
-  squareWidth = canvasWidth / 8;
+    if (mouseButton == RIGHT)
+        return mainBoard.undoMove();
 
-  let [r, c] = mouseToBoardCoords();
 
-  if (!mainBoard.pieceArray[r][c] || mainBoard.currentMove != mainBoard.pieceArray[r][c].colour)
+    squareWidth = canvasWidth / 8;
+
+    let [r, c] = mouseToBoardCoords();
+
+    if (!mainBoard.pieceArray[r][c] || mainBoard.currentMove != mainBoard.pieceArray[r][c].colour)
+        return false;
+
+    legalMovesArr = mainBoard.getLegalMovesSimple(r, c);
+    heldPiece = mainBoard.pieceArray[r][c];
+    heldPiece.r = r; heldPiece.c = c;
+
     return false;
-
-  legalMovesArr = getLegalMovesSimple(mainBoard, r, c);
-  heldPiece = mainBoard.pieceArray[r][c];
-  heldPiece.r = r; heldPiece.c = c;
-
-  return false;
 }
 
 
 function mouseToBoardCoords() {
-  r = (mouseY / squareWidth) >> 0;
-  c = (mouseX / squareWidth) >> 0;
-  return [r, c]
+    r = (mouseY / squareWidth) >> 0;
+    c = (mouseX / squareWidth) >> 0;
+    return [r, c]
 }
 
 
 function mouseReleased() {
-  let [targetR, targetC] = mouseToBoardCoords();
-  if (legalMovesArr.some(([r, c]) => r == targetR && c == targetC)) {
-    heldPiece.hasMoved = true;
-    
-    // castling
-    if (mainBoard.pieceArray[targetR][targetC] &&
-      mainBoard.pieceArray[targetR][targetC].colour == heldPiece.colour) {
-      mainBoard.pieceArray[heldPiece.r][heldPiece.c] = undefined;
-      let rook = mainBoard.pieceArray[targetR][targetC];
-      mainBoard.pieceArray[targetR][targetC] = undefined;
-      let direction = targetC < heldPiece.c ? "-1" : "1";
-      let [kingDest, rookDest] = CASTLINGDESTINATIONS[direction];
-      mainBoard.pieceArray[targetR][rookDest] = rook;
-      mainBoard.pieceArray[targetR][kingDest] = heldPiece;
-    }
-    else {
-      mainBoard.pieceArray[targetR][targetC] = heldPiece;
-      mainBoard.pieceArray[heldPiece.r][heldPiece.c] = undefined;
-    }
-    
-    // en passant
-    if (mainBoard.enPassantTarget && 
-      heldPiece.typeArray[0] == PAWN && 
-      targetR == mainBoard.enPassantTarget[0] &&
-      targetC == mainBoard.enPassantTarget[1])
-      mainBoard.pieceArray[targetR - (mainBoard.currentMove == WHITE ? -1 : 1)][targetC] = undefined;
-    
-    // update en passant target
-    if (heldPiece.typeArray[0] == PAWN && abs(heldPiece.r - targetR) == 2)
-      mainBoard.enPassantTarget = [(heldPiece.r + targetR) / 2, targetC];
-    else
-      mainBoard.enPassantTarget = undefined;
+    let [targetR, targetC] = mouseToBoardCoords();
+    if (!heldPiece)
+        return;
 
-    // promote pawn to queen for now  
-    if (heldPiece.typeArray[0] == PAWN && [0, 7].includes(targetR))
-      mainBoard.pieceArray[targetR][targetC].typeArray[0] = QUEEN;
-
-    // swap the active side
-    mainBoard.currentMove = 1 - mainBoard.currentMove;
-  }
-  
-  heldPiece = undefined;
-  legalMovesArr = [];
+    mainBoard.makeMove(heldPiece.r, heldPiece.c, targetR, targetC);
+    
+    heldPiece = undefined;
+    legalMovesArr = [];
 }
 
 
 function drawLegalMoves() {
-  push();
+    push();
 
-  noStroke();
-  fill(GREENSTROKE);
-  let [r, c] = mouseToBoardCoords();
+    noStroke();
+    fill(GREENSTROKE);
+    let [r, c] = mouseToBoardCoords();
 
-  for (let move of legalMovesArr) {
-    if (move[0] == r && move[1] == c)
-      drawTransparentSquare(r, c);
-    else if (mainBoard.pieceArray[move[0]][move[1]])
-      drawTransparentSquare(...move, true);
-    else
-      circle(
-        move[1] * squareWidth + squareWidth / 2,
-        move[0] * squareWidth + squareWidth / 2,
-        squareWidth / 4
-      );
-  }
+    for (let move of legalMovesArr) {
+        if (move[0] == r && move[1] == c)
+            drawTransparentSquare(r, c);
+        else if (mainBoard.pieceArray[move[0]][move[1]])
+            drawTransparentSquare(...move, true);
+        else
+            circle(
+                move[1] * squareWidth + squareWidth / 2,
+                move[0] * squareWidth + squareWidth / 2,
+                squareWidth / 4
+            );
+    }
 
-  pop();
+    pop();
 }
